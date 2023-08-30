f17f64093de4c939b89e47d4f236fbd7f8fa9522
ximi17 <48459161+ximi17@users.noreply.github.com>
2023-08-30 17:32:27 +1000

    Update App.java

--- app/src/main/java/menumanager_demo/App.java
+++ app/src/main/java/menumanager_demo/App.java
@@ -11,6 +11,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
         System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins");
     }
     
 }
